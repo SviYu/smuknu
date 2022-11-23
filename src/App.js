716949029deaa4faff08400888ea01754326425e
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2022-11-23 21:25:18 +0100

    a

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
         <Navbar cart={cart}/>
       
           <Routes>
-          <Route path='/' element={<Landing />} />
+          <Route path='/smuknu' element={<Landing />} />
             <Route path='/blivmedlem' element={ <BeMember/>} />
             <Route path='/sundhed' element={ <Health/>} />
           <Route path='/produkter' element={<Products AddToCart={ AddToCart} product={product} />} />
