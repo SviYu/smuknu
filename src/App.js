0d599d96ef2243bc0469074f88fb616a0e899e11
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2022-11-24 08:46:53 +0100

    adding one product to cart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import BeMember from './components/BeMember/BeMember';
 import Health from './components/Health/Health';
 import Products from './components/Products/Products';
 import { useEffect, useState } from 'react';
-
 import s from './components/Products/Products.module.css';
 
 function App() {
@@ -28,35 +27,44 @@ function App() {
   /* Shopping Cart */
   const [cart, setCart] = useState([]);
 
-    const AddToCart = () => {
-        setCart(
-            <div>
-                <img src={product.image} alt="Product"/>
-                <div>
-                    <p className={s.productTitle}>
-                        {product.title}
-                    </p>
-                    <p className={s.price}>
-                        {product.price},00 kr.
-                    </p>
-                </div>
-          </div>
-        )
-        console.log('TEST!!!!!!', product.title)
-    }
+  const AddToCart = (product) => {
+    /* const exist = cart.find(x => x.id === product.id);
+    if (exist) {
+      setCart(cart.map(x => x.id === product ? {...exist, qty:exist.qty +1 } : x ))
+    } else {
+      setCart([...cart, {...product, qty: 1}])
+    } */
 
+
+      setCart(
+          <div>
+              <img src={product.image} alt="Product"/>
+              <div>
+                  <p className={s.productTitle}>
+                      {product.title}
+                  </p>
+                  <p className={s.price}>
+                      {product.price},00 kr.
+                  </p>
+              </div>
+        </div>
+      )
+      console.log('TEST!!!!!!', product.title)
+  }
+
+  /* Map of Product Item */
   
 
   return (
     <div className='App'>
       <BrowserRouter>
-        <Navbar cart={cart}/>
+        <Navbar cart={cart} AddToCart={AddToCart}/>
       
           <Routes>
-          <Route path='/smuknu/smuknu' element={<Landing />} />
+          <Route path='/smuknu' element={<Landing />} />
             <Route path='/blivmedlem' element={ <BeMember/>} />
             <Route path='/sundhed' element={ <Health/>} />
-          <Route path='/produkter' element={<Products AddToCart={ AddToCart} product={product} />} />
+          <Route path='/produkter' element={<Products AddToCart={AddToCart} product={product} />} />
           </Routes>
         
       </BrowserRouter>
