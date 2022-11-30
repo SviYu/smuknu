51d8a4568984ea38baebe10f00e9fbc8cf819fba
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2022-11-30 15:16:34 +0100

    Test link with basename

--- src/App.js
+++ src/App.js
@@ -17,10 +17,8 @@ function App() {
   const [product, setProduct] = useState([]);
 
   useEffect(() => {
-      
-    let products = './data/products.json'
     
-        fetch(products)
+        fetch('./data/products.json')
         /* fetch('https://smuknu.webmcdm.dk/products') */
             .then((response) => response.json())
             .then((productData) => {
@@ -89,12 +87,12 @@ function App() {
   return (
     <div className='App'>
       {/* <BrowserRouter basename={config.development ? '/' : '/smuknu'}> */}
-      <BrowserRouter /* basename="smuknu" */>
+      <BrowserRouter basename="smuknu">
         <Navbar cart={cart} AddToCart={AddToCart} renderCart={renderCart} />
       
           <Routes>
           {/* <Route path={config.development ? '/' : '/smuknu'} element={<Landing />} /> */}
-          <Route path='/smuknu' element={<Landing />} />
+          <Route path='/' element={<Landing />} />
           <Route path='/blivmedlem' element={ <BeMember/>} />
           <Route path='/sundhed' element={ <Health/>} />
           <Route path='/produkter' element={<Products AddToCart={AddToCart} product={product} />} />
